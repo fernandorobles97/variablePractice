0f9b1a248d3cc9262bd24669bf8bb37e1bce8008
Fernando Robles <fernandorobles97@yahoo.com>
2023-02-04 21:26:07 -0600

    Add 3 final variables

--- variables.js
+++ variables.js
@@ -4,4 +4,8 @@ var username = "helloworld99"
 
 var ageMinimum = 18
 var catAmount = 3
-var mileCount = 6          
+var mileCount = 6         
+
+var isOnline = true
+var isCool = false
+var hasDrip = true 

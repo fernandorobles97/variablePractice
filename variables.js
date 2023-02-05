e6a28448f38ca535bda51ee12eab222d67d58bf0
Fernando Robles <fernandorobles97@yahoo.com>
2023-02-04 22:51:38 -0600

    Add console.log statements

--- variables.js
+++ variables.js
@@ -9,3 +9,11 @@ var mileCount = 6
 var isOnline = true
 var isCool = false
 var hasDrip = true 
+
+email = "goodbyeworld@gmail.com"
+password = "worldgoodbye"
+username = "goodbyeworld99"
+
+console.log(email)
+console.log(password)
+console.log(hasDrip)
